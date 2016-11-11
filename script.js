3cc201a6386cddd17d19d34cf9561f816a743f6f
root <root@deb.deb>
2016-11-11 19:05:31 +0300

    sprintf

--- script.js
+++ script.js
@@ -12,22 +12,29 @@
 
 
 
+
+
+
+
+
+
+
+/*
 // The string multipling functions
 String.prototype.dup = function() {
     var val = this.valueOf();
     return val + val;
-};
+}
 
 String.prototype.repeat = function(n) {
     if (!n || n <= 0) return "";
     if (!this.origin) this.origin = this.valueOf();
     var val = this.repeat(parseInt(n / 2));
     return ((n % 2) ? this.origin : "") + val + val;
-};
+}
 
 // The padding functions
-String.prototype.padding = function(n, c)
-{
+String.prototype.padding = function(n, c) {
     if (!c) c = " ";
     if (c.length > 1) c = c.charAt(0);
     var val = this.valueOf();
@@ -37,17 +44,17 @@ String.prototype.padding = function(n, c)
     return pad + val;
 }
 
-String.prototype.leftPad = function(n, c)
-{
+String.prototype.leftPad = function(n, c) {
     return this.padding(Math.abs(n), c);
 }
 
-String.prototype.rightPad = function(n, c)
-{
+String.prototype.rightPad = function(n, c) {
     return this.padding(-Math.abs(n), c);
 }
 
 
+console.log("123".padding(6, " "));
+*/
 
 
 
@@ -60,6 +67,146 @@ String.prototype.rightPad = function(n, c)
 
 
 
+
+
+
+
+
+
+
+
+
+
+function sprintf( ) {   // Return a formatted string
+    //
+    // +   original by: Ash Searle (http://hexmen.com/blog/)
+    // + namespaced by: Michael White (http://crestidg.com)
+ 
+    var regex = /%%|%(\d+\$)?([-+#0 ]*)(\*\d+\$|\*|\d+)?(\.(\*\d+\$|\*|\d+))?([scboxXuidfegEG])/g;
+    var a = arguments, i = 0, format = a[i++];
+ 
+    // pad()
+    var pad = function(str, len, chr, leftJustify) {
+        var padding = (str.length >= len) ? '' : Array(1 + len - str.length >>> 0).join(chr);
+        return leftJustify ? str + padding : padding + str;
+    };
+ 
+    // justify()
+    var justify = function(value, prefix, leftJustify, minWidth, zeroPad) {
+        var diff = minWidth - value.length;
+        if (diff > 0) {
+            if (leftJustify || !zeroPad) {
+            value = pad(value, minWidth, ' ', leftJustify);
+            } else {
+            value = value.slice(0, prefix.length) + pad('', diff, '0', true) + value.slice(prefix.length);
+            }
+        }
+        return value;
+    };
+ 
+    // formatBaseX()
+    var formatBaseX = function(value, base, prefix, leftJustify, minWidth, precision, zeroPad) {
+        // Note: casts negative numbers to positive ones
+        var number = value >>> 0;
+        prefix = prefix && number && {'2': '0b', '8': '0', '16': '0x'}[base] || '';
+        value = prefix + pad(number.toString(base), precision || 0, '0', false);
+        return justify(value, prefix, leftJustify, minWidth, zeroPad);
+    };
+ 
+    // formatString()
+    var formatString = function(value, leftJustify, minWidth, precision, zeroPad) {
+        if (precision != null) {
+            value = value.slice(0, precision);
+        }
+        return justify(value, '', leftJustify, minWidth, zeroPad);
+    };
+ 
+    // finalFormat()
+    var doFormat = function(substring, valueIndex, flags, minWidth, _, precision, type) {
+        if (substring == '%%') return '%';
+ 
+        // parse flags
+        var leftJustify = false, positivePrefix = '', zeroPad = false, prefixBaseX = false;
+        for (var j = 0; flags && j < flags.length; j++) switch (flags.charAt(j)) {
+            case ' ': positivePrefix = ' '; break;
+            case '+': positivePrefix = '+'; break;
+            case '-': leftJustify = true; break;
+            case '0': zeroPad = true; break;
+            case '#': prefixBaseX = true; break;
+        }
+ 
+        // parameters may be null, undefined, empty-string or real valued
+        // we want to ignore null, undefined and empty-string values
+        if (!minWidth) {
+            minWidth = 0;
+        } else if (minWidth == '*') {
+            minWidth = +a[i++];
+        } else if (minWidth.charAt(0) == '*') {
+            minWidth = +a[minWidth.slice(1, -1)];
+        } else {
+            minWidth = +minWidth;
+        }
+ 
+        // Note: undocumented perl feature:
+        if (minWidth < 0) {
+            minWidth = -minWidth;
+            leftJustify = true;
+        }
+ 
+        if (!isFinite(minWidth)) {
+            throw new Error('sprintf: (minimum-)width must be finite');
+        }
+ 
+        if (!precision) {
+            precision = 'fFeE'.indexOf(type) > -1 ? 6 : (type == 'd') ? 0 : void(0);
+        } else if (precision == '*') {
+            precision = +a[i++];
+        } else if (precision.charAt(0) == '*') {
+            precision = +a[precision.slice(1, -1)];
+        } else {
+            precision = +precision;
+        }
+ 
+        // grab value using valueIndex if required?
+        var value = valueIndex ? a[valueIndex.slice(0, -1)] : a[i++];
+ 
+        switch (type) {
+            case 's': return formatString(String(value), leftJustify, minWidth, precision, zeroPad);
+            case 'c': return formatString(String.fromCharCode(+value), leftJustify, minWidth, precision, zeroPad);
+            case 'b': return formatBaseX(value, 2, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+            case 'o': return formatBaseX(value, 8, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+            case 'x': return formatBaseX(value, 16, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+            case 'X': return formatBaseX(value, 16, prefixBaseX, leftJustify, minWidth, precision, zeroPad).toUpperCase();
+            case 'u': return formatBaseX(value, 10, prefixBaseX, leftJustify, minWidth, precision, zeroPad);
+            case 'i':
+            case 'd': {
+                        var number = parseInt(+value);
+                        var prefix = number < 0 ? '-' : positivePrefix;
+                        value = prefix + pad(String(Math.abs(number)), precision, '0', false);
+                        return justify(value, prefix, leftJustify, minWidth, zeroPad);
+                    }
+            case 'e':
+            case 'E':
+            case 'f':
+            case 'F':
+            case 'g':
+            case 'G':
+                        {
+                        var number = +value;
+                        var prefix = number < 0 ? '-' : positivePrefix;
+                        var method = ['toExponential', 'toFixed', 'toPrecision']['efg'.indexOf(type.toLowerCase())];
+                        var textTransform = ['toString', 'toUpperCase']['eEfFgG'.indexOf(type) % 2];
+                        value = prefix + Math.abs(number)[method](precision);
+                        return justify(value, prefix, leftJustify, minWidth, zeroPad)[textTransform]();
+                    }
+            default: return substring;
+        }
+    };
+ 
+    return format.replace(regex, doFormat);
+}
+
+//console.log(sprintf("ss %05d", 1));
 
 
 
@@ -180,20 +327,3 @@ setInterval(onupdate_tmr, 1000);
 
 //sprintf("12345", 5);
 
-var x = "123".padding(6);
-console.log(x);
-
-
-Number.prototype.radix = function(r, n, c)
-{
-    if (!c) c = '0';
-    return this.toString(r).padding(n, c);
-}
-
-Number.prototype.dec = function(n, c) {
-    return this.radix(0x0A, (n) ? n : 2, c);
-};
-
-var s111 = 250;
-
-console.log( s111.dec(6,0));
